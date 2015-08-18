4be196026ea273f75f95d36e1c502c672babfe7f
Mario Meltzow <dev@meltzow.net>
2015-08-18 12:45:30 +0200

    initial project structure

---
+++ core/src/main/java/brainless/openrts/SimpleMain.java
@@ -0,0 +1,11 @@
+package brainless.openrts;
+
+public class SimpleMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
