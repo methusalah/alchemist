19d9a08b15cff49cb7116de3d8ea3ea783aed20e
methusalah <ben.oeilnoir@gmail.com>
2015-11-27 00:44:57 +0100

    work on heightmap flatdata optimisation

--- core/src/main/java/model/world/terrain/heightmap/HeightMap.java
+++ core/src/main/java/model/world/terrain/heightmap/HeightMap.java
@@ -5,6 +5,8 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import model.world.terrain.atlas.AtlasLayer;
+import util.LogUtil;
 import util.geometry.geom2d.Point2D;
 import util.geometry.geom3d.Point3D;
 import util.geometry.structure.grid.Grid;
@@ -19,22 +21,55 @@ public class HeightMap extends Grid<HeightMapNode> {
 	
 	public HeightMap(@JsonProperty("width")int width,
 			@JsonProperty("height")int height,
-			@JsonProperty("flatData")double[] flatData,
+			@JsonProperty("flatData")String flatData,
 			@JsonProperty("coord")Point2D coord) {
 		super(width, height, coord);
-		for(int i = 0; i < xSize*ySize; i++)
-			set(i, new HeightMapNode(i, flatData[i]));
+		
+		int index = 0;
+		for (int i = 0; i < width * height; i++) {
+			if(flatData.charAt(index) == '-'){
+				int nbZero = getIntFromHexString(flatData.substring(index+1, index+9));
+				LogUtil.info(flatData.substring(index+1, index+9)+"zeros found : "+nbZero);
+				while(nbZero-- > 0)
+					set(i++, new HeightMapNode(i, 0));
+				i--;
+				index += 10;
+			} else {
+				float f = getFloatFromHexString(flatData.substring(index, index+8));
+				set(i, new HeightMapNode(i, f));
+				index += 9;
+			}
+		}
+
+//		for(int i = 0; i < xSize*ySize; i++)
+//			set(i, new HeightMapNode(i, flatData[i]));
 	}
 	
 	public Point3D getPos(HeightMapNode height){
 		return new Point3D(getCoord(height.getIndex()), height.getElevation());
 	}
 
-	public double[] getFlatData(){
-		double[] res = new double[xSize*ySize];
-		for(int i = 0; i < xSize*ySize; i++)
-			res[i] = get(i).getElevation();
-		return res;
+	public String getFlatData(){
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < size(); i++){
+			float f = (float)(get(i).getElevation());
+			if(f == 0){
+				// grouping of zero values
+				int nbZero = 1;
+				while(i+nbZero < size() &&
+						nbZero < Integer.MAX_VALUE &&
+						(float)(get(i+nbZero)).getElevation() == 0){
+					nbZero++;
+				}
+				if(nbZero > 4){
+					sb.append("-"+String.format("%08X,", nbZero));
+					i = i+nbZero-1;
+					continue;
+				}
+			}
+			sb.append(String.format("%08X,", f));
+		}
+		return sb.toString();
 	}
 	
 	public int getWidth(){
@@ -50,4 +85,26 @@ public class HeightMap extends Grid<HeightMapNode> {
 	public List<HeightMapNode> getAll() {
 		return super.getAll();
 	}
+	
+	public static float getFloatFromHexString(String s) {
+	    return (float)((Character.digit(s.charAt(0), 16) << 28) +
+	    		(Character.digit(s.charAt(1), 16) << 24) + 
+	    		(Character.digit(s.charAt(2), 16) << 20) + 
+	    		(Character.digit(s.charAt(3), 16) << 16) +
+	    		(Character.digit(s.charAt(4), 16) << 12) + 
+	    		(Character.digit(s.charAt(5), 16) << 8) + 
+	    		(Character.digit(s.charAt(6), 16) << 4) + 
+	    		Character.digit(s.charAt(7), 16));
+	}
+	public static int getIntFromHexString(String s) {
+	    return (int)((Character.digit(s.charAt(0), 16) << 28) +
+	    		(Character.digit(s.charAt(1), 16) << 24) + 
+	    		(Character.digit(s.charAt(2), 16) << 20) + 
+	    		(Character.digit(s.charAt(3), 16) << 16) +
+	    		(Character.digit(s.charAt(4), 16) << 12) + 
+	    		(Character.digit(s.charAt(5), 16) << 8) + 
+	    		(Character.digit(s.charAt(6), 16) << 4) + 
+	    		Character.digit(s.charAt(7), 16));
+	}
+
 }
