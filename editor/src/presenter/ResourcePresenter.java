4e49634ef6871b2f277137e177040d50793156b1
methusalah <ben.oeilnoir@gmail.com>
2016-01-08 21:18:31 +0100

    Blueprint are nos sorted in the ressource tab, and saved blueprint erase the last version properly

--- editor/src/presenter/ResourcePresenter.java
+++ editor/src/presenter/ResourcePresenter.java
@@ -19,16 +19,25 @@ public class ResourcePresenter {
 		blueprintList = new SimpleListProperty<Blueprint>(FXCollections.observableArrayList());
 		for(Blueprint bp : BlueprintLibrary.getAllBlueprints())
 			blueprintList.add(bp);
+		blueprintList.sort(new BlueprintComparator());
 	}
 	
-	public ListProperty<Blueprint> blueprintListProperty(){
+	public ListProperty<Blueprint> getBlueprintListProperty(){
 		return blueprintList;
 	}
 	
 	public void saveEntity(EntityNode ep){
 		Blueprint saved = getBlueprintFromPresenter(ep);
 		BlueprintLibrary.saveBlueprint(saved);
+		
+		for(Blueprint bp : blueprintList)
+			if(bp.getName().equalsIgnoreCase(saved.getName())){
+				blueprintList.set(blueprintList.indexOf(bp), saved);
+				return;
+			}
+		// at this point, the saved blueprint doesn't replace any existing blueprint
 		blueprintList.add(saved);
+		blueprintList.sort(new BlueprintComparator());
 	}
 	
 	private Blueprint getBlueprintFromPresenter(EntityNode ep){
